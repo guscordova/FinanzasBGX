17bc788a98a46bfbe5aadb599b33b5516132d496
robertogarza <robertogarza@10.13.155.186>
2013-12-05 00:15:37 -0600

    se cambia dataModel a list

--- FinanzasBGX.Web/src/java/jsf/VentaController.java
+++ FinanzasBGX.Web/src/java/jsf/VentaController.java
@@ -1,5 +1,7 @@
 package jsf;
 
+import dto.ModeloPorProducir;
+import dto.VentaDistribuidor;
 import dto.VentaMes;
 import entities.Venta;
 import jsf.util.JsfUtil;
@@ -57,11 +59,11 @@ public class VentaController implements Serializable {
     private double totalSalesYear = 0.0;
     private double totalSalesActualMonth = 0.0;
     
-    private DataModel totalSalesMonth = null;
-    private DataModel totalSalesDistributorYear = null;
-    private DataModel totalSalesDistributorCompare = null;
-    private DataModel totalSalesDistributorCompare2 = null;
-    private DataModel totalDifference = null;
+    private List<VentaMes> totalSalesMonth = null;
+    private List<VentaDistribuidor> totalSalesDistributorYear = null;
+    private List<VentaMes> totalSalesDistributorCompare = null;
+    private List<VentaMes> totalSalesDistributorCompare2 = null;
+    private List<VentaMes> totalDifference = null;
     
     private GoogleChartModel chartModelCurrentYear = new DefaultGoogleChartModel("LineChart");
     private GoogleChartModel chartModelPastYears = new DefaultGoogleChartModel("LineChart");
@@ -74,19 +76,17 @@ public class VentaController implements Serializable {
     private double pendienteCobrarAnual = 0.0;
     private String pendienteCobrarViewCurrentDistributor;
     private String pendienteCobrarViewSearch;
-    private DataModel pendienteCobrarDistribuidor = null;
+    private List<VentaDistribuidor> pendienteCobrarDistribuidor = null;
     
     private double pendienteProducirAnual = 0.0;
     private String pendienteProducirViewCurrentModel;
     private String pendienteProducirViewSearch;
-    private DataModel pendienteProducirComponent = null;
-    
+    private List<ModeloPorProducir> pendienteProducirComponent = null;
     
     private Venta current;
     private DataModel items = null;
     private PaginationHelper pagination;
     private int selectedItemIndex;
-    
     
     public VentaController() {
         this.monthViewCurrentYear = Calendar.getInstance().get(Calendar.YEAR) + "";
@@ -111,29 +111,29 @@ public class VentaController implements Serializable {
     
     @PostConstruct
     public void init() {
-        this.totalSalesMonth = new ListDataModel(this.getFacade().getMonthSales(Integer.parseInt(monthViewCurrentYear), -1));
-        this.totalSalesDistributorYear = new ListDataModel(this.getFacade().getDistributorSales(distributorViewStartDate, distributorViewEndDate, -1));
+        this.totalSalesMonth = this.getFacade().getMonthSales(Integer.parseInt(monthViewCurrentYear), -1);
+        this.totalSalesDistributorYear = this.getFacade().getDistributorSales(distributorViewStartDate, distributorViewEndDate, -1);
         List<VentaMes> distributor1 = this.getFacade().getMonthSalesByDate(compareViewStartDate, compareViewEndDate, -1);
         List<VentaMes> distributor2 = this.getFacade().getMonthSalesByDate(compareViewStartDate, compareViewEndDate, -1);
         List<VentaMes> difference = new ArrayList<VentaMes>();
-        this.totalSalesDistributorCompare = new ListDataModel(distributor1);
-        this.totalSalesDistributorCompare2 = new ListDataModel(distributor2);
+        this.totalSalesDistributorCompare = distributor1;
+        this.totalSalesDistributorCompare2 = distributor2;
         for (int i = 0; i < distributor1.size(); i++) {
             VentaMes diff = new VentaMes();
             diff.setTotal(distributor1.get(i).getTotal() - distributor2.get(i).getTotal());
             difference.add(diff);
         }
-        this.totalDifference = new ListDataModel(difference);
+        this.totalDifference = difference;
         
         DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
         this.monthViewSearch = "Busqueda por año " + monthViewCurrentYear + ", distribuidor Todos";
         this.distributorViewSearch = "Busqueda entre " +  df.format(distributorViewStartDate) + " y " + df.format(distributorViewEndDate) + ", distribuidor Todos";
         this.compareViewSearch = "Busqueda entre " +  df.format(compareViewStartDate) + " y " + df.format(compareViewEndDate) + ", comparando el distribuidor Todos con el distribuidor Todos";
         
-        this.pendienteCobrarDistribuidor = new ListDataModel(this.getFacade().getPendienteCobrar(-1));
+        this.pendienteCobrarDistribuidor = this.getFacade().getPendienteCobrar(-1);
         this.pendienteCobrarViewSearch = "Busqueda por distribuidor Todos";        
         
-        this.pendienteProducirComponent = new ListDataModel(this.getFacade().getPendienteProducir(-1));
+        this.pendienteProducirComponent = this.getFacade().getPendienteProducir(-1);
         this.pendienteProducirViewSearch = "Busqueda por modelo Todos";
         
         loadGraphMonths(graphViewCurrentYear);
@@ -276,37 +276,37 @@ public class VentaController implements Serializable {
         return this.pendienteProducirAnual;
     }
 
-    public DataModel getTotalSalesMonth(){
+    public List<VentaMes> getTotalSalesMonth(){
         return totalSalesMonth;
     }
     
-    public DataModel getTotalSalesDistributorYear(){
+    public List<VentaDistribuidor> getTotalSalesDistributorYear(){
         return totalSalesDistributorYear;
     }
     
-    public DataModel getTotalSalesDistributorCompare(){
+    public List<VentaMes> getTotalSalesDistributorCompare(){
         return totalSalesDistributorCompare;
     }
     
-    public DataModel getTotalSalesDistributorCompare2(){
+    public List<VentaMes> getTotalSalesDistributorCompare2(){
         return totalSalesDistributorCompare2;
     }
     
-    public DataModel getTotalDifference() {
+    public List<VentaMes> getTotalDifference() {
         return totalDifference;
     }
     
-    public DataModel getPendienteCobrarDistribuidor() {
+    public List<VentaDistribuidor> getPendienteCobrarDistribuidor() {
         return pendienteCobrarDistribuidor;
     }
     
-    public DataModel getPendienteProducirComponent() {
+    public List<ModeloPorProducir> getPendienteProducirComponent() {
         return pendienteProducirComponent;
     }
     
     public String searchMonth()
     {
-        this.totalSalesMonth = new ListDataModel(this.getFacade().getMonthSales(Integer.parseInt(monthViewCurrentYear), Integer.parseInt(monthViewCurrentDistributor)));
+        this.totalSalesMonth = this.getFacade().getMonthSales(Integer.parseInt(monthViewCurrentYear), Integer.parseInt(monthViewCurrentDistributor));
         this.monthViewSearch = "Busqueda por año " + monthViewCurrentYear +
                                ", distribuidor " + this.getFacade().getDistributorNameById(Integer.parseInt(monthViewCurrentDistributor));        
         return "";
@@ -314,7 +314,7 @@ public class VentaController implements Serializable {
     
     public String searchDistributor()
     {
-        this.totalSalesDistributorYear = new ListDataModel(this.getFacade().getDistributorSales(distributorViewStartDate, distributorViewEndDate, Integer.parseInt(distributorViewCurrentDistributor)));
+        this.totalSalesDistributorYear = this.getFacade().getDistributorSales(distributorViewStartDate, distributorViewEndDate, Integer.parseInt(distributorViewCurrentDistributor));
         DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
         this.distributorViewSearch = "Busqueda entre " +  df.format(distributorViewStartDate) + " y " + df.format(distributorViewEndDate) +
                                      ", distribuidor " + this.getFacade().getDistributorNameById(Integer.parseInt(distributorViewCurrentDistributor));        
@@ -331,9 +331,9 @@ public class VentaController implements Serializable {
             diff.setTotal(distributor1.get(i).getTotal() - distributor2.get(i).getTotal());
             difference.add(diff);
         }
-        this.totalSalesDistributorCompare = new ListDataModel(distributor1);
-        this.totalSalesDistributorCompare2 = new ListDataModel(distributor2);
-        this.totalDifference = new ListDataModel(difference);
+        this.totalSalesDistributorCompare = distributor1;
+        this.totalSalesDistributorCompare2 = distributor2;
+        this.totalDifference = difference;
         DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
         this.compareViewSearch = "Busqueda entre " +  df.format(compareViewStartDate) + " y " + df.format(compareViewEndDate) + 
                                  ", comparando el distribuidor " + this.getFacade().getDistributorNameById(Integer.parseInt(compareViewCurrentDistributor)) + 
@@ -343,13 +343,13 @@ public class VentaController implements Serializable {
 
     public String searchPendienteCobrar()
     {
-        this.pendienteCobrarDistribuidor = new ListDataModel(this.getFacade().getPendienteCobrar(Integer.parseInt(pendienteCobrarViewCurrentDistributor)));
+        this.pendienteCobrarDistribuidor = this.getFacade().getPendienteCobrar(Integer.parseInt(pendienteCobrarViewCurrentDistributor));
         this.pendienteCobrarViewSearch = "Busqueda por distribuidor " + this.getFacade().getDistributorNameById(Integer.parseInt(pendienteCobrarViewCurrentDistributor));        
         return "";
     }
     
     public String searchPendienteProducir() {
-        this.pendienteProducirComponent = new ListDataModel(this.getFacade().getPendienteProducir(Integer.parseInt(pendienteProducirViewCurrentModel)));
+        this.pendienteProducirComponent = this.getFacade().getPendienteProducir(Integer.parseInt(pendienteProducirViewCurrentModel));
         this.pendienteProducirViewSearch = "Busqueda por modelo " + this.getFacade().getModelNameById(Integer.parseInt(pendienteProducirViewCurrentModel));   
         return "";
     }
@@ -434,212 +434,7 @@ public class VentaController implements Serializable {
         return chartModelPastYears;
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    public Venta getSelected() {
-        if (current == null) {
-            current = new Venta();
-            selectedItemIndex = -1;
-        }
-        return current;
-    }
-
      private VentaFacade getFacade() {
         return ejbFacade;
-    }
-
-    public PaginationHelper getPagination() {
-        if (pagination == null) {
-            pagination = new PaginationHelper(10) {
-                @Override
-                public int getItemsCount() {
-                    return getFacade().count();
-                }
-
-                @Override
-                public DataModel createPageDataModel() {
-                    return new ListDataModel(getFacade().findRange(new int[]{getPageFirstItem(), getPageFirstItem() + getPageSize()}));
-                }
-            };
-        }
-        return pagination;
-    }
-
-    public String prepareList() {
-        recreateModel();
-        return "List";
-    }
-
-    public String prepareView() {
-        current = (Venta) getItems().getRowData();
-        selectedItemIndex = pagination.getPageFirstItem() + getItems().getRowIndex();
-        return "View";
-    }
-
-    public String prepareCreate() {
-        current = new Venta();
-        selectedItemIndex = -1;
-        return "Create";
-    }
-
-    public String create() {
-        try {
-            getFacade().create(current);
-            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("VentaCreated"));
-            return prepareCreate();
-        } catch (Exception e) {
-            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-            return null;
-        }
-    }
-
-    public String prepareEdit() {
-        current = (Venta) getItems().getRowData();
-        selectedItemIndex = pagination.getPageFirstItem() + getItems().getRowIndex();
-        return "Edit";
-    }
-
-    public String update() {
-        try {
-            getFacade().edit(current);
-            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("VentaUpdated"));
-            return "View";
-        } catch (Exception e) {
-            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-            return null;
-        }
-    }
-
-    public String destroy() {
-        current = (Venta) getItems().getRowData();
-        selectedItemIndex = pagination.getPageFirstItem() + getItems().getRowIndex();
-        performDestroy();
-        recreatePagination();
-        recreateModel();
-        return "List";
-    }
-
-    public String destroyAndView() {
-        performDestroy();
-        recreateModel();
-        updateCurrentItem();
-        if (selectedItemIndex >= 0) {
-            return "View";
-        } else {
-            // all items were removed - go back to list
-            recreateModel();
-            return "List";
-        }
-    }
-
-    private void performDestroy() {
-        try {
-            getFacade().remove(current);
-            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("VentaDeleted"));
-        } catch (Exception e) {
-            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-        }
-    }
-
-    private void updateCurrentItem() {
-        int count = getFacade().count();
-        if (selectedItemIndex >= count) {
-            // selected index cannot be bigger than number of items:
-            selectedItemIndex = count - 1;
-            // go to previous page if last page disappeared:
-            if (pagination.getPageFirstItem() >= count) {
-                pagination.previousPage();
-            }
-        }
-        if (selectedItemIndex >= 0) {
-            current = getFacade().findRange(new int[]{selectedItemIndex, selectedItemIndex + 1}).get(0);
-        }
-    }
-
-    public DataModel getItems() {
-        if (items == null) {
-            items = getPagination().createPageDataModel();
-        }
-        return items;
-    }
-    
-    private void recreateModel() {
-        items = null;
-    }
-
-    private void recreatePagination() {
-        pagination = null;
-    }
-
-    public String next() {
-        getPagination().nextPage();
-        recreateModel();
-        return "List";
-    }
-
-    public String previous() {
-        getPagination().previousPage();
-        recreateModel();
-        return "List";
-    }
-
-    public SelectItem[] getItemsAvailableSelectMany() {
-        return JsfUtil.getSelectItems(ejbFacade.findAll(), false);
-    }
-
-    public SelectItem[] getItemsAvailableSelectOne() {
-        return JsfUtil.getSelectItems(ejbFacade.findAll(), true);
-    }
-
-    public Venta getVenta(java.lang.Integer id) {
-        return ejbFacade.find(id);
-    }
-
-    @FacesConverter(forClass = Venta.class)
-    public static class VentaControllerConverter implements Converter {
-
-        @Override
-        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
-            if (value == null || value.length() == 0) {
-                return null;
-            }
-            VentaController controller = (VentaController) facesContext.getApplication().getELResolver().
-                    getValue(facesContext.getELContext(), null, "ventaController");
-            return controller.getVenta(getKey(value));
-        }
-
-        java.lang.Integer getKey(String value) {
-            java.lang.Integer key;
-            key = Integer.valueOf(value);
-            return key;
-        }
-
-        String getStringKey(java.lang.Integer value) {
-            StringBuilder sb = new StringBuilder();
-            sb.append(value);
-            return sb.toString();
-        }
-
-        @Override
-        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
-            if (object == null) {
-                return null;
-            }
-            if (object instanceof Venta) {
-                Venta o = (Venta) object;
-                return getStringKey(o.getId());
-            } else {
-                throw new IllegalArgumentException("object " + object + " is of type " + object.getClass().getName() + "; expected type: " + Venta.class.getName());
-            }
-        }
     }
 }
